57b51555d916251dc19e7cb980c3d83877d238fc
unknown <obyedov@gmail.com>
2020-05-07 12:55:01 +0200

    first steps

---
+++ base.js
@@ -0,0 +1,13 @@
+//переменные
+const name = 'Vitaliy'
+const lastName = 'Obyedov' // string
+let age = 29 // number
+console.log(name)
+//name = 'Vi' //Uncaught TypeError: Assignment to constant variable.
+const isProgrammer = true // boolean
+console.log(isProgrammer)
+age = 'some string' // послкольку это динамически типизируемый язык и в своем корне не строгой типизации поэтому не будет ошибкой так делать
+//тем не менее в джаваскрипт присутствуют различные типы данных и их всего 8
+console.log(age)
+
+
