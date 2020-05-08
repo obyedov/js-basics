e782a93fe9122f1fb3b64c06995e4006daaac7cc
unknown <obyedov@gmail.com>
2020-05-08 10:15:50 +0200

    1 variables

--- base.js
+++ base.js
@@ -1,13 +1,20 @@
-//переменные
-const name = 'Vitaliy'
+// 1 переменные
+const firstname = 'Vitaliy'
 const lastName = 'Obyedov' // string
-let age = 29 // number
-console.log(name)
+const age = 29 // number
+//console.log(firstname)
 //name = 'Vi' //Uncaught TypeError: Assignment to constant variable.
 const isProgrammer = true // boolean
-console.log(isProgrammer)
-age = 'some string' // послкольку это динамически типизируемый язык и в своем корне не строгой типизации поэтому не будет ошибкой так делать
+//console.log(isProgrammer)
+//age = 'some string' // послкольку это динамически типизируемый язык и в своем корне не строгой типизации поэтому не будет ошибкой так делать
 //тем не менее в джаваскрипт присутствуют различные типы данных и их всего 8
-console.log(age)
+//console.log(age)
+const _ = 'private'
+const $ = 'some value'
+// const if = 'fdfswf'//Uncaught SyntaxError: Unexpected token 'if'
+const withNum5 = '5'
+//const 5withNum = '5'//Uncaught SyntaxError: Invalid or unexpected token
 
-
+// 2 Мутирование
+console.log(`Имя человека ${firstname}`)
+console.log('Фамилия человека ' + lastName)
